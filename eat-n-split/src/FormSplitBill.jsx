200c2616f3354275121fb0be597e543b3d742ce4
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-03-19 21:09:17 +0900

    支払い額の入力フィールドを追加し、ユーザーと友達の支払い額を計算して表示する機能を実装。支払い者の選択機能も追加。

--- eat-n-split/src/FormSplitBill.jsx
+++ eat-n-split/src/FormSplitBill.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState("");
+  const [userPayment, setUserPayment] = useState("");
+  const friendpayment = bill ? bill - userPayment : "";
+  const [whoPays, setWhoPays] = useState("user");
+
   return (
     <form className="form-split-bill">
       <h2>{selectedFriend.name}さんと割り勘！</h2>
 
       <label>💰 支払い額</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>🧍🏻 あなたの支払い額</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={userPayment}
+        onChange={(e) =>
+          setUserPayment(
+            Number(e.target.value) > bill ? userPayment : Number(e.target.value)
+          )
+        }
+      />
 
       <label>👬 {selectedFriend.name}さんの支払い</label>
-      <input type="text" disabled />
+      <input type="text" value={friendpayment} disabled />
 
       <label>😳 お支払いは？</label>
-      <select>
+      <select value={whoPays} onChange={(e) => setWhoPays(e.target.value)}>
         <option value="user">自分</option>
         <option value="friend">{selectedFriend.name}さん</option>
       </select>
